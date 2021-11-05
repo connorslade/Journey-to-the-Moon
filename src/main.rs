d521fd65c392f2eee6335fc5fdbd8ff3b094711c
Connor Slade <connor@connorcode.com>
2021-11-05 11:36:31 -0400

    🥨 Update Config Parser

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use std::env;
 use std::fs;
 
-use simple_config_parser::config::Config;
+use simple_config_parser::Config;
 
 #[macro_use]
 mod common;
@@ -23,22 +23,22 @@ fn main() {
 
     println!("[*] Starting Journey to the Moon (V{})", VERSION);
 
-    let cfg = time_print!("[*] Loading Config", || {
-        let mut cfg = Config::new(Some("data/config.cfg"));
-        cfg.read().ok().expect("Error reading the config file");
-        cfg
-    });
+    let cfg = time_print!("[*] Loading Config", || Config::new()
+        .file("data/config.cfg")
+        .expect("Error reading the config file"));
+
     let raw_data = time_print!("[*] Reading Tree", || fs::read_to_string(TREE_PATH)
         .unwrap()
         .replace('\r', ""));
+
     let data = time_print!("[*] Parsing Tree", || Question::parse_json(raw_data)
         .unwrap());
 
     unsafe { DATA = Some(data.clone()) }
 
     if web_server {
-        let ip = cfg.get("ip").unwrap();
-        let port = cfg.get("port").unwrap().parse::<u16>().unwrap();
+        let ip = cfg.get_str("ip").unwrap();
+        let port = cfg.get::<u16>("port").unwrap();
 
         println!("[*] Starting Server ({}:{})\n", ip, port);
         server::start_server(&ip, port);
